a2bc09fd788e0654f9e22d9a6ce56d83baf9d3a9
robin <sonirobin369@gmail.com>
2020-10-03 23:37:43 +0530

    adding the call back to see the asynchronous behaviour

--- async.js
+++ async.js
@@ -2,19 +2,34 @@
 // start something now and finish later
 // let's add the eventlistener
 
+const getToDo = (callback) =>{
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState ===4 && request.status === 200){ // see MDN for finding out - why ?
+            callback(undefined,request.responseText); 
+        }else if(request.readyState===4){
+            callback('unsuccessful request',request.responseText);
+        }
+    })
 
-const request = new XMLHttpRequest();
+    request.open('GET','https://jsonplaceholder.typicode.com/todos/')
+    request.send()
+}
+console.log('1')
+console.log(2)
 
-request.addEventListener('readystatechange', () => {
-    if(request.readyState ===4){ // see MDN for finding out - why ?
-        console.log(request.responseText) 
+getToDo((err,data) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
     }
 })
-
-request.open('GET','https://jsonplaceholder.typicode.com/todos/')
-request.send()
-
+console.log(3)
+console.log(4)
 // we can see the result in the networc section of the browser 
 // but in the code we don't cnow when it is complete and how to get access to this data
 // that we will do using the evenlistner
 console.log('sent')
+
+// Asynchronous behaviourf
